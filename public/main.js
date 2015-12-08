5801ada20c60f5e85d83e0e759191349ab04fcde
Adam Beck <AdamB.Infinity@gmail.com>
2015-12-08 01:21:51 -0500

    Minor tweak

--- public/main.js
+++ public/main.js
@@ -5,8 +5,14 @@
 
   button.addEventListener('click', function() {
     var request = new XMLHttpRequest();
-    //request.open('GET', 'http://nodeapp/api', true);
-    request.open('GET', 'http://172.17.0.2/api', true);
+    // can get this to work (by attaching to the container and chaning the url to match 
+    // the ip in the docker environment variable)
+    // request.open('GET', 'http://172.17.0.2/api', true);
+
+    // cannot get this to work
+    // get a CORS error even though the app works with the above configuration and the server
+    // has CORS enabled
+    request.open('GET', 'http://nodeapp/api', true);
 
     request.onload = function() {
       if (request.status >= 200 && request.status < 400) {

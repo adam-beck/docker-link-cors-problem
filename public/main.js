1cd306d4b5d729b571c7eca2c54d1822e047fda0
Adam Beck <AdamB.Infinity@gmail.com>
2015-12-08 01:40:05 -0500

    Update main.js

--- public/main.js
+++ public/main.js
@@ -5,13 +5,13 @@
 
   button.addEventListener('click', function() {
     var request = new XMLHttpRequest();
-    // can get this to work (by attaching to the container and chaning the url to match 
+    // can get this to work (by attaching to the container and changing the url to match 
     // the ip in the docker environment variable)
     // request.open('GET', 'http://172.17.0.2/api', true);
 
     // cannot get this to work
     // get a CORS error even though the app works with the above configuration and the server
-    // has CORS enabled
+    // has CORS enabled; doesn't even seem to be hitting the server
     request.open('GET', 'http://nodeapp/api', true);
 
     request.onload = function() {
